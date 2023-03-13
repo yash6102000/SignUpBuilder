5e9027efd8ed0275041cc5bcf3d931707f713c86
yash6102000 <yash.thakur@copods.co>
2023-03-13 22:39:17 +0530

    fix:font

--- packages/main-app/src/styleConfigs.tsx
+++ packages/main-app/src/styleConfigs.tsx
@@ -4,14 +4,8 @@ export const fontSize = [
     { value: "base", label: "16px" },
     { value: "lg", label: "18px" },
     { value: "xl", label: "20px" },
-    { value: "2xl", label: "24px" },
     { value: "3xl", label: "30px" },
-    { value: "4xl", label: "36px" },
-    { value: "5xl", label: "48px" },
-    { value: "6xl", label: "60px" },
-    { value: "7xl", label: "72px" },
-    { value: "8xl", label: "96px" },
-    { value: "9xl", label: "128px" },
+   
   ];
   export const fontWeight = [
     { value: "100", label: "100" },

3c679de6438cffed1191e91df5786a07e2215af1
yash6102000 <yash.thakur@copods.co>
2023-03-14 16:32:26 +0530

    types-added

--- packages/stencil-library/src/interface.tsx
+++ packages/stencil-library/src/interface.tsx
@@ -46,7 +46,7 @@ export interface SubmitButton {
   fontWeight: string;
   fontColor: string;
   backgroundColor: string;
-  borderRadius: 0;
+  borderRadius: number;
   buttonState: string;
 }
 export interface Colors{
